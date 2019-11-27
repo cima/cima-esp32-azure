ce3e981b25caf8e5e4782b6b709d1361c25ebd5f
Martin Šimek <martin.simek@zf.com>
2019-11-27 14:46:18 +0100

    Two threads writing to log. Compilable, uploadable & runnable!

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ https://cdn-shop.adafruit.com/datasheets/BST-BME280_DS001-10.pdf
 ### ESP 4.0 (beta 2)
 - [Getting started ESP 4.0 (beta 2)](https://docs.espressif.com/projects/esp-idf/en/v4.0-beta2/get-started/index.html)
 - [ESP-IDF](https://github.com/espressif/esp-idf)
-
+```
     git clone -b release/v4.0 --recursive https://github.com/espressif/esp-idf.git
-
+```
 - [ESP-toolchain 4.0 beta 2 Installer](https://docs.espressif.com/projects/esp-idf/en/v4.0-beta2/get-started/windows-setup.html)
   - python 3.6+
   - cmake 3.13
@@ -32,10 +32,10 @@ https://cdn-shop.adafruit.com/datasheets/BST-BME280_DS001-10.pdf
 
 ### Azure IoT SDK C for ESP
 - [ESP Azure IoT SDK](https://github.com/espressif/esp-azure)
-
+```
     git clone --recursive https://github.com/espressif/esp-azure.git
     git submodule update --init --recursive
-
+```
 ### BOOST
 - [Getting started with boost](https://www.boost.org/doc/libs/1_71_0/more/getting_started/windows.html)
 - Ensure environment variable *BOOST_ROOT* that contain path to unpacked boost distribution.
@@ -45,6 +45,7 @@ https://cdn-shop.adafruit.com/datasheets/BST-BME280_DS001-10.pdf
 - Adafruit drivers for Arduino
   - Patch adafruit driver
 - [Disable FreeRTOS static allocation API](https://esp32.com/viewtopic.php?t=3504) set in [sdkconfig.defaults](sdkconfig.defaults)
+- [Quartz & Bitrate settings](https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html#get-started-connect)
 
 ### Adafruit drivers
 
--- main/CimaEsp32Agent.cpp
+++ main/CimaEsp32Agent.cpp
@@ -16,6 +16,10 @@
 #include "esp_log.h"
 #include "esp_sleep.h"
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
+#include <freertos/task.h>
+
 cima::system::Log logger("main");
 
 std::string ssid     = "<your wifi SSID here>";
@@ -34,11 +38,16 @@ cima::iot::IoTHubManager iotHubManager(connectionString, certificate);
 #define SENDING_INTERVAL 10000
 #define DEVICE_ID "Esp32Device"
 #define MESSAGE_MAX_LEN 256
+#define STACK_SIZE 2048
 
 const char *MESSAGE_TEMPLATE = "{\"greetings\":\"Hello world!\", \"Temperature\":%f, \"Humidity\":%f}";
 
+void loop(void * pvParameters);
+void hello(void * pvParameters);
+
 static bool messageSending = true;
 static uint64_t last_send_ms;
+static TaskHandle_t xHandle = NULL;
 
 int64_t millis(){
   int64_t time_since_boot = esp_timer_get_time();
@@ -56,13 +65,19 @@ extern "C" void app_main(void)
   wifiManager.start();
   
   logger.info(" > IoT Hub");
-  iotHubManager.init();
+  //iotHubManager.init(); //TODO
 
   logger.info(" > Wire");
-  wireManager.init();
+  //wireManager.init(); //TODO
 
   logger.info(" > Environment sensor");
-  environmentSensorManager.init();
+  //environmentSensorManager.init(); //TODO
+
+  //TODO looping thread
+  
+  xTaskCreate(hello, "Hello G", STACK_SIZE, (char *)"Gizmo", tskIDLE_PRIORITY, &xHandle);
+  xTaskCreate(hello, "Hello S", STACK_SIZE, (char *)"Sheep", tskIDLE_PRIORITY, NULL);
+  
 
   last_send_ms = millis();
 
@@ -70,7 +85,14 @@ extern "C" void app_main(void)
   //randomSeed(analogRead(0));
 }
 
-void loop() {
+void hello(void * pvParameters) {
+  while(true){
+    logger.info(":-) Hello %s :-)", (char *)pvParameters);
+    vTaskDelay(1000 / portTICK_RATE_MS);
+  }
+}
+
+void loop(void * pvParameters) {
   if (wifiManager.isStarted()) {
     if (messageSending && 
         (millis() - last_send_ms) >= SENDING_INTERVAL &&
